0ce4a8f6a7eed85f7cf1e23afdcf856855d36484
Mykel Koded <koded@ixit.com.ng>
2018-07-23 21:57:58 +0100

    stupid typo

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -4,12 +4,12 @@ import PropTypes from 'prop-types'
 import './cards.css'
 
 //9010f072421c44f492f167d8ba2a6951
-// https://openexchangerates.org/api/latest.json?app_id= ?symbols=NGN
+// https://openexchangerates.org/api/latest.json?app_id= ?symbols
 
 const TrainingCard = props => (
   <div className="card">
     <div className="card-body">
-      <h5 className="card-title">{props.cardMetaadding l.cardTitle}</h5>
+      <h5 className="card-title">{props.cardMeta.cardTitle}</h5>
       <p className="card-text">{props.cardMeta.description}</p>
     </div>
     <div className="card-footer">
